8c1ec2590f227135da696f6ddd76e8042b277ee3
Jhgggggg <134745520+Jhgggggg@users.noreply.github.com>
2023-12-12 16:38:50 -0300

    Update Boyfriend.hx

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -9,7 +9,7 @@ using StringTools;
 
 class Boyfriend extends Character
 {
-	public var startedDeath:Bool = false;
+	public var startedDeath:Bool = true;
 
 	public function new(x:Float, y:Float, ?char:String = 'bf')
 	{
